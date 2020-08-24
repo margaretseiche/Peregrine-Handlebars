b989c348acf0621048ed18fb8793aab7ac08584c
Margaret Seiche <margaretseiche@hotmail.com>
2020-08-23 20:32:32 -0400

    add signup file and routes in server

--- server.js
+++ server.js
@@ -56,7 +56,10 @@ app.get("/lunches", function(req, res) {
   });
 });
 
-
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+require("./routes/post-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
